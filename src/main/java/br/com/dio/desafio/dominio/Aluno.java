6eb37cb181038b00722e27161054b5bbf4c9762f
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-10-26 12:51:06 -0300

    feat: implementação inicial das classes

--- src/main/java/br/com/dio/desafio/dominio/Aluno.java
+++ src/main/java/br/com/dio/desafio/dominio/Aluno.java
@@ -1,5 +1,33 @@
 package br.com.dio.desafio.dominio;
 
+import java.io.ObjectStreamClass;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Aluno {
   private String nome;
+  private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+  private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+  public Aluno(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() { return nome; }
+
+  public Set<Conteudo> getConteudosInscritos() { return conteudosInscritos; }
+  
+  public Set<Conteudo> getConteudosConcluidos() { return conteudosConcluidos; }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Aluno aluno = (Aluno) o;
+
+    return Objects.equals(nome, aluno.nome)
+        && Objects.equals(conteudosInscritos, aluno.conteudosInscritos)
+        && Objects.equals(conteudosConcluidos, aluno.conteudosConcluidos);
+  }
 }
--- src/main/java/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/main/java/br/com/dio/desafio/dominio/Bootcamp.java
@@ -1,5 +1,46 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+
 public class Bootcamp {
+  private String nome;
+  private String descricao;
+  private final LocalDate dataInicial = LocalDate.now();
+  private final LocalDate dataFinal = dataInicial.plusDays(45);
+  private Set<Aluno> alunosInscritos = new LinkedHashSet<>();
+  private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+  public Bootcamp(String nome, String descricao) {
+    this.nome = nome;
+    this.descricao = descricao;
+  }
+
+  public String getNome() { return nome; }
+
+  public String getDescricao() { return descricao; }
   
+  public LocalDate getDataInicial() { return dataInicial; }
+  
+  public LocalDate getDataFinal() { return dataFinal; }
+  
+  public Set<Aluno> getAlunosInscritos() { return alunosInscritos; }
+  
+  public Set<Conteudo> getConteudos() { return conteudos; }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Bootcamp bootcamp = (Bootcamp) o;
+
+    return Objects.equals(nome, bootcamp.nome)
+        && Objects.equals(descricao, bootcamp.descricao)
+        && Objects.equals(dataInicial, bootcamp.dataInicial)
+        && Objects.equals(dataFinal, bootcamp.dataFinal)
+        && Objects.equals(alunosInscritos, bootcamp.alunosInscritos)
+        && Objects.equals(conteudos, bootcamp.conteudos);
+  }
 }
