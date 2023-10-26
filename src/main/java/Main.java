0a5b19e90e257dad2db9df3d05e007a2a6c82cab
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-10-26 12:14:09 -0300

    refactor: utilização do método toString()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,9 @@ import br.com.dio.desafio.dominio.Mentoria;
 public class Main {
   public static void main(String[] args) {
     Curso curso = new Curso("Java 8", "Curso Java 8 da DIO", 5);
-    System.out.println(curso.getTitulo());
-    System.out.println(curso.getDescricao());
-    System.out.println(curso.getCargaHoraria());
-
     Mentoria mentoria = new Mentoria("Mentoria Java Spring Boot", "Mentoria sobre Java Spring Boot", LocalDate.now());
-    System.out.println(mentoria.getTitulo());
-    System.out.println(mentoria.getDescricao());
-    System.out.println(mentoria.getData());
+
+    System.out.println(curso);
+    System.out.println(mentoria);
   }
 }
