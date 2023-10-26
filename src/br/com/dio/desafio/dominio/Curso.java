29f17008a5299ff0e34177473fc3394b2ea63a74
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-10-26 11:27:00 -0300

    feat: implementação inicial das classes

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,5 +1,19 @@
 package br.com.dio.desafio.dominio;
 
 public class Curso {
-  
+  private String titulo;
+  private String descricao;
+  private int cargaHoraria;
+
+  public Curso(String titulo, String descricao, int cargaHoraria) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.cargaHoraria = cargaHoraria;
+  }
+
+  public String getTitulo() { return titulo; }
+
+  public String getDescricao() { return descricao; }
+
+  public int getCargaHoraria() { return cargaHoraria; }
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,5 +1,21 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Mentoria {
-  
+  private String titulo;
+  private String descricao;
+  private LocalDate data;
+
+  public Mentoria(String titulo, String descricao, LocalDate data) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.data = data;
+  }
+
+  public String getTitulo() { return titulo; }
+
+  public String getDescricao() { return descricao; }
+
+  public LocalDate getData() { return data; }
 }
