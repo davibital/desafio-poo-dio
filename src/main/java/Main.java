1c453340672e762b76ab80dc15f4a40736c50922
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-10-26 13:00:45 -0300

    feat: teste dos novos métodos da aplicação

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Aluno;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -6,7 +9,12 @@ public class Main {
   public static void main(String[] args) {
     Curso curso = new Curso("Java 8", "Curso Java 8 da DIO", 5);
     Mentoria mentoria = new Mentoria("Mentoria Java Spring Boot", "Mentoria sobre Java Spring Boot", LocalDate.now());
+    Bootcamp bootcamp = new Bootcamp("Bootcamp TQI Backend Developer", "Bootcamp para conhecimentos na área do backend com Java e Kotlin");
+    Aluno aluno = new Aluno("Davi");
 
+    System.out.println(bootcamp.getNome());
+    System.out.println(bootcamp.getDescricao());
+    System.out.println(aluno.getNome());
     System.out.println(curso);
     System.out.println(mentoria);
   }
